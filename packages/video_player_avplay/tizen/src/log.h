7c7a05a6154025f8a9aa5d528336561581b4a10f
Markus H <92981432+mhoeckner@users.noreply.github.com>
2024-11-13 11:57:49 +0100

    logging to cli

--- packages/video_player_avplay/tizen/src/log.h
+++ packages/video_player_avplay/tizen/src/log.h
@@ -3,6 +3,8 @@
 
 #include <dlog.h>
 
+#include <iostream>
+
 #ifdef LOG_TAG
 #undef LOG_TAG
 #endif
@@ -12,9 +14,8 @@
 #define __MODULE__ strrchr("/" __FILE__, '/') + 1
 #endif
 
-#define LOG(prio, fmt, arg...)                                         \
-  dlog_print(prio, LOG_TAG, "%s: %s(%d) > " fmt, __MODULE__, __func__, \
-             __LINE__, ##arg)
+#define LOG(prio, fmt, arg...) \
+  printf("\n %s: %s(%d) > " fmt, __MODULE__, __func__, __LINE__, ##arg)
 
 #define LOG_DEBUG(fmt, args...) LOG(DLOG_DEBUG, fmt, ##args)
 #define LOG_INFO(fmt, args...) LOG(DLOG_INFO, fmt, ##args)
